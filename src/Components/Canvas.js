7d8c00e95aa19e7211b7a0bfccc2739ea80a63b7
Marqui <98498681+Marqui-13@users.noreply.github.com>
2022-10-18 18:40:38 -0700

    Update Canvas.js

--- src/Components/Canvas.js
+++ src/Components/Canvas.js
@@ -43,21 +43,18 @@ const Canvas = () => {
 window.addEventListener("touchstart", event => {
     mouse.x = event.changedTouches[0].clientX;
     mouse.y = event.changedTouches[0].clientY;
-    drawing = true;
 }, false);
 
 window.addEventListener("touchmove", event => {
     //event.preventDefault();
     mouse.x = event.targetTouches[0].clientX;
     mouse.y = event.targetTouches[0].clientY;
-    branchOut();
 }, false);
 
 window.addEventListener("touchend", event => {
     //event.preventDefault();
     mouse.x = 0;
     mouse.y = 0;
-    drawing = false;
 }, false);
 // END JAVASCRIPT TOUCH EVENTS
         
